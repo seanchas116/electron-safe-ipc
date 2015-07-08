36cdd5fff9df270a26592795184fe722330b2b40
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-08 11:32:54 +0900

    Separate mainWindow file

--- test/main.js
+++ test/main.js
@@ -1,16 +1,11 @@
+"use strict";
+
 var path = require("path");
 var app = require("app");
-var BrowserWindow = require("browser-window");
 var Mocha = require("mocha");
 
 app.on("ready", function () {
-  var mainWindow = new BrowserWindow({
-    width: 100,
-    height: 100,
-    //show: false,
-    "node-integration": false,
-  });
-  mainWindow.loadUrl("file://" + __dirname + "/index.html");
+  var mainWindow = require("./mainWindow");
 
   var mocha = new Mocha();
   mocha.addFile(path.join(__dirname, "test/test.js"));
