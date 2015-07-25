301b95e8f9aef8eeec3975adb897c5b75f4ce6d8
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-25 13:08:45 +0900

    Hide window on test

--- test/mainWindow.js
+++ test/mainWindow.js
@@ -4,7 +4,7 @@ var BrowserWindow = require("browser-window");
 var mainWindow = new BrowserWindow({
   width: 100,
   height: 100,
-  //show: false,
+  show: false,
   "node-integration": false,
 });
 mainWindow.loadUrl("file://" + __dirname + "/index.html");
